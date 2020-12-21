868d041f9358ee01b81472e9cb1dec570883ab7e
Sonya <sofa.chernikova@mail.ru>
2020-12-21 18:54:02 +0300

    Task №1

--- ind.js
+++ ind.js
@@ -1,5 +1,17 @@
 // Задание 1
 
+console.log('Задание 1')
+let number = 9
+let string = 'Chernikova' 
+let bull = true
+let nothing = null
+
+console.log(string + number) //string + int == string
+console.log(string + bull) //string + boolean == string
+console.log(string + nothing) //string + null == string
+console.log(number + bull) //int + boolean == int
+console.log(number + null) //int + null == int
+console.log(bull + null) //boolean + null == int
 
 // Задание 2
 
